f460d1e1f00ff5f39112bad513c68ba7aeb5edb4
xcheng-tsinghua <786682191@qq.com>
2025-07-25 19:51:35 +0800

    change

--- models/cstpnt.py
+++ models/cstpnt.py
@@ -43,26 +43,32 @@ class SurfknnPntattn(nn.Module):
 
 
 class UpSample(nn.Module):
-    def __init__(self, mlp):  # fp1: in_channel=150, mlp=[128, 128]
+    def __init__(self, mlp):
         super().__init__()
         self.mlp = utils.MLP(1, mlp)
 
     def forward(self, xyz1, xyz2, points1, points2):
+        """
+        xyz1: [bs, 3, n_point1]
+        xyz2: [bs, 3, n_point2]
+        points1: [bs, channel1, n_point1]
+        points2: [bs, channel2, n_point2]
+        """
         xyz1 = xyz1.permute(0, 2, 1)
         xyz2 = xyz2.permute(0, 2, 1)
         points2 = points2.permute(0, 2, 1)
 
-        bs, n_point, channel = xyz1.shape
+        bs, n_point, _ = xyz1.shape
 
         dists = utils.square_distance(xyz1, xyz2)
 
         dists, idx = dists.sort(dim=-1)
-        dists, idx = dists[:, :, :3], idx[:, :, :3]  # [B, N, 3]
+        dists, idx = dists[:, :, :3], idx[:, :, :3]
 
         dist_recip = 1.0 / (dists + 1e-8)
         norm = torch.sum(dist_recip, dim=2, keepdim=True)
 
-        weight = dist_recip / norm  # ->[B, N, 3]
+        weight = dist_recip / norm
         interpolated_points = torch.sum(utils.index_points(points2, idx) * weight.view(bs, n_point, 3, 1), dim=2)
 
         # skip link concatenation
@@ -80,10 +86,12 @@ class CstPnt(nn.Module):
         super().__init__()
 
         self.sa1 = SurfknnPntattn(int(n_points_all * drate), 50, n_stepk, 3, 128)
-        self.sa2 = SurfknnPntattn(int(n_points_all * drate ** 2), 100, n_stepk, 128, 256)
+        self.sa2 = SurfknnPntattn(int(n_points_all * drate ** 2), 75, n_stepk, 128, 256)
+        self.sa3 = SurfknnPntattn(int(n_points_all * drate ** 3), 100, n_stepk, 256, 512)
 
-        self.fp2 = UpSample((256 + 128, 256, 256, 128))  # in_channel = points2_channel + points1_channel
-        self.fp1 = UpSample((128 + 6, 128, 128, 128))
+        self.fp3 = UpSample((512 + 256, 512, 256))
+        self.fp2 = UpSample((256 + 128, 256, 128))
+        self.fp1 = UpSample((128 + 6, 128, 128))
 
         self.mlp_fea = utils.MLP(1, (128, int((128 * n_embout) ** 0.5), n_embout))
 
@@ -100,7 +108,9 @@ class CstPnt(nn.Module):
         # Set Abstraction layers
         l1_xyz, l1_points = self.sa1(xyz, xyz)
         l2_xyz, l2_points = self.sa2(l1_xyz, l1_points)
+        l3_xyz, l3_points = self.sa3(l2_xyz, l2_points)
 
+        l2_points = self.fp3(l2_xyz, l3_xyz, l2_points, l3_points)
         l1_points = self.fp2(l1_xyz, l2_xyz, l1_points, l2_points)
         l0_points = self.fp1(xyz, l1_xyz, torch.cat([xyz, xyz], 1), l1_points)
 
@@ -108,17 +118,11 @@ class CstPnt(nn.Module):
         feat = self.mlp_fea(l0_points)
         ex_features = feat.permute(0, 2, 1)  # [bs, n_points_all, self.n_embout]
 
-        ex_features = ex_features.transpose(-1, -2)
-        # [bs, self.n_embout, n_points_all]
+        ex_features = ex_features.transpose(-1, -2)  # [bs, self.n_embout, n_points_all]
 
-        mad = self.mlp_mad(ex_features).transpose(-1, -2)
-        # [bs, n_points_all, 3]
-
-        adj = self.mlp_adj(ex_features).transpose(-1, -2)
-        # [bs, n_points_all, 2]
-
-        pt = self.mlp_pt(ex_features).transpose(-1, -2)
-        # [bs, n_points_all, 7]
+        mad = self.mlp_mad(ex_features).transpose(-1, -2)  # [bs, n_points_all, 3]
+        adj = self.mlp_adj(ex_features).transpose(-1, -2)  # [bs, n_points_all, 2]
+        pt = self.mlp_pt(ex_features).transpose(-1, -2)  # [bs, n_points_all, 4]
 
         adj_log = F.log_softmax(adj, dim=-1)
         pt_log = F.log_softmax(pt, dim=-1)
@@ -132,10 +136,6 @@ if __name__ == '__main__':
 
     _mad, _adj_log, _pt_log = anet(test_tensor)
     print(_mad.size(), _adj_log.size(), _pt_log.size())
-
-
-
-
 
 
 
